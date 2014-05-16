5b4f4e4c9ffb1090471372b07d30b55a58990b24
birkin <birkin.diana@gmail.com>
2014-05-16 10:06:04 -0400

    adds cron-call file/function.

--- utils/cron_call.py
+++ utils/cron_call.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-""" Triggers svn update job.
-    Call like: /path/to/iip_stuff/env_iip/bin/python2.6 /path/to/iip_stuff/iip/iip_search_app/utils/cron_svn_update.py.
+""" Triggers git pull job.
+    Call like: /path/to/env/bin/python2.6 /path/to/usep_gh_handler_app/utils/cron_call.py
     Note that output is not logged to the normal application log, but to the rqworker log (assuming one exists). """
 
 import redis, rq
@@ -9,6 +9,6 @@ import redis, rq
 
 q = rq.Queue( u'usep', connection=redis.Redis() )
 job = q.enqueue_call (
-    func=u'iip_search_app.models.run_call_svn_update',
+    func=u'usep_gh_handler_app.utils.processor.run_call_git_pull',
     kwargs = {}
     )
