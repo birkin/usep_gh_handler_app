d2c7bc5604e5377317ed221ce98f36d91a2e809b
birkin <birkin.diana@gmail.com>
2014-05-28 09:44:46 -0400

    enhances worker start script.

--- utils/queue_utils/worker_start.sh
+++ utils/queue_utils/worker_start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 
-rqworker --name "usep"
+USEP_WORKER_NAME=usep_worker_$RANDOM
+USEP_WORKER_LOG_FILENAME=$usep_gh__LOG_DIR/$USEP_WORKER_NAME.log
+USEP_QUEUE_NAME="usep"
+
+echo "worker name: " $USEP_WORKER_NAME
+echo "log filename: " $USEP_WORKER_LOG_FILENAME
+echo "queue name: " $USEP_QUEUE_NAME
+
+rqworker --name $USEP_WORKER_NAME $USEP_QUEUE_NAME >> $USEP_WORKER_LOG_FILENAME 2>&1 &
+# rqworker --name $USEP_WORKER_NAME $USEP_QUEUE_NAME
