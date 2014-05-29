54ad0b4fefb6e3cf9c7b085fb95f98b9f5fb9869
birkin <birkin.diana@gmail.com>
2014-05-29 16:19:40 -0400

    fixes log filename.

--- utils/logger_setup.py
+++ utils/logger_setup.py
@@ -10,7 +10,7 @@ def setup_logger():
     """ Returns a logger to write to a file. """
     LOG_DIR = unicode( os.environ.get(u'usep_gh__LOG_DIR') )
     LOG_LEVEL = unicode( os.environ.get(u'usep_gh__LOG_LEVEL') )
-    filename = u'%s/useh_gh_handler.log' % LOG_DIR
+    filename = u'%s/usep_gh_handler.log' % LOG_DIR
     formatter = logging.Formatter( u'[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s' )
     logger = logging.getLogger( __name__ )
     # logger = logging.getLogger( u'usep_gh_handler' )
