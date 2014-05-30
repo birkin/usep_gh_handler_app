57838710125117a9562ca002985181562b480f80
birkin <birkin.diana@gmail.com>
2014-05-30 11:04:36 -0400

    adds copy/remove initial code.

--- utils/processor.py
+++ utils/processor.py
@@ -13,33 +13,6 @@ class ProcessorUtils( object ):
         self.GIT_CLONED_DIR_PATH = unicode( os.environ.get(u'usep_gh__GIT_CLONED_DIR_PATH') )
         self.log = log
 
-    ## git pull ##
-
-    def call_git_pull( self ):
-        """ Runs git_pull.
-                Returns list of filenames.
-            Called by run_call_git_pull(). """
-        original_directory = os.getcwd()
-        os.chdir( self.GIT_CLONED_DIR_PATH )
-        command = u'git pull'
-        r = envoy.run( command.encode(u'utf-8') )  # envoy requires strings
-        self._log_command_output( r )
-        os.chdir( original_directory )
-        return
-
-    def _log_command_output( self, envoy_response ):
-        """ Creates and returns dict of envoy_response attributes.
-            Called by call_git_pull(). """
-        return_dict = {
-            u'status_code': envoy_response.status_code,  # int
-            u'std_out': envoy_response.std_out.decode(u'utf-8'),
-            u'std_err': envoy_response.std_err.decode(u'utf-8'),
-            u'command': envoy_response.command,  # list
-            u'history': envoy_response.history  # list
-            }
-        self.log.info( u'in utils.Processor._log_command_output(); envoy_output, `%s`' % return_dict )
-        return return_dict
-
     ## handler web-app helpers ##
 
     def log_github_post( self, flask_request ):
@@ -73,17 +46,52 @@ class ProcessorUtils( object ):
             files_to_process[u'to_remove'] = commit_info[u'commits'][u'removed']
         return files_to_process
 
-    # def prep_data_dict( self, flask_request_data ):
-    #     """ Prepares the data-dict to be sent to run_call_git_pull().
-    #         Called by usep_gh_handler.handle_github_push() """
-    #     self.log.debug( u'in processor.prep_data_dict(); flask_request_data, `%s`' % flask_request_data )
-    #     data = { u'added': [], u'modified': [], u'removed': [], u'timestamp': unicode(datetime.datetime.now()) }
-    #     if flask_request_data:
-    #         commit_info = json.loads( flask_request_data )
-    #         data[u'added'] = commit_info[u'commits'][u'added']
-    #         data[u'modified'] = commit_info[u'commits'][u'modified']
-    #         data[u'removed'] = commit_info[u'commits'][u'removed']
-    #     return data
+    ## git pull ##
+
+    def call_git_pull( self ):
+        """ Runs git_pull.
+                Returns list of filenames.
+            Called by run_call_git_pull(). """
+        original_directory = os.getcwd()
+        os.chdir( self.GIT_CLONED_DIR_PATH )
+        command = u'git pull'
+        r = envoy.run( command.encode(u'utf-8') )  # envoy requires strings
+        self._log_command_output( r )
+        os.chdir( original_directory )
+        return
+
+    def _log_command_output( self, envoy_response ):
+        """ Creates and returns dict of envoy_response attributes.
+            Called by call_git_pull(). """
+        return_dict = {
+            u'status_code': envoy_response.status_code,  # int
+            u'std_out': envoy_response.std_out.decode(u'utf-8'),
+            u'std_err': envoy_response.std_err.decode(u'utf-8'),
+            u'command': envoy_response.command,  # list
+            u'history': envoy_response.history  # list
+            }
+        self.log.info( u'in utils.Processor._log_command_output(); envoy_output, `%s`' % return_dict )
+        return return_dict
+
+    ## files_to_process
+
+    def get_files_to_copy( self, files_to_process ):
+        """ Creates and returns list of filepaths to copy. """
+        if files_to_process[u'to_copy'] == [ u'all' ]:
+            filepaths_to_copy = self.make_complete_files_to_copy_list()  # TODO
+        else:
+            filepaths_to_copy = files_to_process[u'to_copy']
+        self.log.debug( u'in utils.Processor.get_files_to_copy(); filepaths_to_copy, `%s`' % filepaths_to_copy )
+        return filepaths_to_copy
+
+    def get_files_to_remove( self, files_to_process ):
+        """ Creates and returns list of filepaths to remove. """
+        if files_to_process[u'to_remove'] == [ u'check' ]:
+            filepaths_to_remove = self.make_complete_files_to_remove_list()  # TODO
+        else:
+            filepaths_to_remove = files_to_process[u'to_remove']
+        self.log.debug( u'in utils.Processor.get_files_to_remove(); filepaths_to_remove, `%s`' % filepaths_to_remove )
+        return filepaths_to_remove
 
     ## end class ProcessorUtils()
 
@@ -100,17 +108,32 @@ def run_call_git_pull( files_to_process ):
     """ Initiates a git pull update.
             Spawns a call to Processor.process_file() for each result found.
         Called by usep_gh_handler.handle_github_push(). """
-    print u'- HERE01'
-    assert sorted( files_to_process.keys() ) == [ u'timestamp', u'to_copy', u'to_remove' ], sorted( github_file_info.keys() )
-    time.sleep( 2 )  # let any existing jobs in process finish
     log = logger_setup.setup_logger()
-    log.debug( u'in processor.run_call_git_pull(); files_to_process, `%s`' % pprint.pformat(files_to_process) )
+    assert sorted( files_to_process.keys() ) == [ u'timestamp', u'to_copy', u'to_remove' ]; log.debug( u'in processor.run_call_git_pull(); files_to_process, `%s`' % pprint.pformat(files_to_process) )
+    time.sleep( 2 )  # let any existing jobs in process finish
     processor_utils = ProcessorUtils( log )
     processor_utils.call_git_pull()
-    # job = q.enqueue_call(
-    #     func=u'usep_gh_handler_app.utils.xyz',
-    #     kwargs = {} )
+    ( files_to_copy, files_to_remove ) = ( processor_utils.get_files_to_copy(files_to_process), processor_utils.get_files_to_remove(files_to_process) )
+    for filepath in files_to_copy:
+        job = q.enqueue_call( func=u'usep_gh_handler_app.utils.processor.run_copy_file', kwargs = {u'filepath': filepath} )
+    for filepath in files_to_remove:
+        job = q.enqueue_call( func=u'usep_gh_handler_app.utils.processor.run_remove_file', kwargs = {u'filepath': filepath} )
     return
+
+# def run_call_git_pull( files_to_process ):
+#     """ Initiates a git pull update.
+#             Spawns a call to Processor.process_file() for each result found.
+#         Called by usep_gh_handler.handle_github_push(). """
+#     assert sorted( files_to_process.keys() ) == [ u'timestamp', u'to_copy', u'to_remove' ], sorted( github_file_info.keys() )
+#     time.sleep( 2 )  # let any existing jobs in process finish
+#     log = logger_setup.setup_logger()
+#     log.debug( u'in processor.run_call_git_pull(); files_to_process, `%s`' % pprint.pformat(files_to_process) )
+#     processor_utils = ProcessorUtils( log )
+#     processor_utils.call_git_pull()
+#     # job = q.enqueue_call(
+#     #     func=u'usep_gh_handler_app.utils.xyz',
+#     #     kwargs = {} )
+#     return
 
 # def run_process_inscription( kwargs ):
 #     """ Stub; TODO, build out.
