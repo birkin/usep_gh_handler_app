0024dc1dd7ea8d1e9a73ddf3100306bd2a472aa9
birkin <birkin.diana@gmail.com>
2016-01-29 11:27:26 -0500

    stubs new indexing code.

--- utils/indexer.py
+++ utils/indexer.py
@@ -3,7 +3,7 @@
 import os, pprint
 import redis, rq, solr
 from usep_gh_handler_app.utils import log_helper
-from usep_gh_handler_app.utils.indexer_parser import Parser
+# from usep_gh_handler_app.utils.indexer_parser import Parser
 
 
 class Indexer( object ):
@@ -14,65 +14,80 @@ class Indexer( object ):
     def __init__( self, log ):
         """ Settings. """
         self.log = log
-        self.solr_dict = {}
+        # self.solr_dict = {}
         self.worthwhile_dirs = [ u'bib_only', u'metadata_only', u'transcribed' ]  # only need to update index for these dirs
         self.WEBSERVED_DATA_DIR_PATH = unicode( os.environ.get(u'usep_gh__WEBSERVED_DATA_DIR_PATH') )
         self.SOLR_URL = unicode( os.environ.get(u'usep_gh__SOLR_URL') )
-        self.BIB_XML_PATH = unicode( os.environ.get(u'usep_gh__BIB_XML_PATH') )
+        # self.BIB_XML_PATH = unicode( os.environ.get(u'usep_gh__BIB_XML_PATH') )
 
     ## update index entry ##
 
     def update_index_entry( self, filename ):
         """ Updates solr index for a new or changed file.
-            Called by run_update_index().
-            TODO: replace pq extracts with straight lxml or beautifulsoup extracts. """
+            Called by run_update_index() """
         self.log.debug( u'in utils.indexer.update_index_entry(); filename, `%s`' % filename )
         full_file_path = u'%s/inscriptions/%s' % ( self.WEBSERVED_DATA_DIR_PATH, filename )
-        self._build_solr_dict( full_file_path, self.BIB_XML_PATH )
-        self._post_solr_update()
+        doc = self._build_solr_doc( full_file_path )
+        self._post_solr_update( doc )
         return
 
-    def _build_solr_dict( self, inscription_xml_path, bib_xml_path ):
-        """ Calls parser to build the solr dict.
-            Called by update_index_entry(). """
-        p = Parser( inscription_xml_path, bib_xml_path, self.log )
-        self.solr_dict[u'id'] = p.i_id; assert not p.i_id == None
-        self.solr_dict[u'bib_ids'] = p.bib_ids
-        self.solr_dict[u'bib_ids_filtered'] = p.parseBibIdsFiltered()
-        self.solr_dict[u'bib_ids_types'] = p.parseBibIdsTypes()
-        self.solr_dict[u'title'] = p.parseTitle()
-        self.solr_dict[u'text_genre'] = p.parse_text_genre()
-        self.solr_dict[u'object_type'] = p.parse_object_type()
-        self.solr_dict[u'bib_titles'] = p.parseBibTitles()
-        self.solr_dict[u'bib_titles_all'] = p.bib_titles_all
-        self.solr_dict[u'bib_authors'] = p.parseBibAuthors()
-        self.solr_dict[u'condition'] = p.parseCondition()
-        self.solr_dict[u'decoration'] = p.parseDecoration()
-        self.solr_dict[u'fake'] = p.parseFake()
-        self.solr_dict[u'graphic_name'] = p.parse_graphic_name()
-        self.solr_dict[u'language'] = p.parseLanguage()
-        self.solr_dict[u'material'] = p.parseMaterial()
-        self.solr_dict[u'msid_region'] = p.parseMsidRegion()
-        self.solr_dict[u'msid_settlement'] = p.parseMsidSettlement()
-        self.solr_dict[u'msid_institution'] = p.parseMsidInstitution()
-        self.solr_dict[u'msid_repository'] = p.parseMsidRepository()
-        self.solr_dict[u'msid_idno'] = p.parseMsidIdno()
-        self.solr_dict[u'status'] = p.parseStatus()
-        self.solr_dict[u'writing'] = p.parseWriting()
-        self.log.debug( u'in utils.indexer.Indexer._build_solr_dict(); solr_dict, `%s`' % pprint.pformat(self.solr_dict) )
-        return self.solr_dict
+    def _build_solr_doc( self, inscription_xml_path ):
+        return 'foo'
 
-    def _post_solr_update( self ):
-        """ Updates existing solr entry.
-            Called by update_index_entry(). """
-        self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); self.SOLR_URL is: `%s`' % self.SOLR_URL )
-        s = solr.Solr( self.SOLR_URL )
-        response = s.add( self.solr_dict )
-        s.commit()
-        s.close()
-        self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); post complete; response is: %s' % response )
-        # return
-        return response
+    def _post_solr_update( self, doc ):
+        return 'foo2'
+
+    # def update_index_entry( self, filename ):
+    #     """ Updates solr index for a new or changed file.
+    #         Called by run_update_index().
+    #         TODO: replace pq extracts with straight lxml or beautifulsoup extracts. """
+    #     self.log.debug( u'in utils.indexer.update_index_entry(); filename, `%s`' % filename )
+    #     full_file_path = u'%s/inscriptions/%s' % ( self.WEBSERVED_DATA_DIR_PATH, filename )
+    #     self._build_solr_dict( full_file_path, self.BIB_XML_PATH )
+    #     self._post_solr_update()
+    #     return
+
+    # def _build_solr_dict( self, inscription_xml_path, bib_xml_path ):
+    #     """ Calls parser to build the solr dict.
+    #         Called by update_index_entry(). """
+    #     p = Parser( inscription_xml_path, bib_xml_path, self.log )
+    #     self.solr_dict[u'id'] = p.i_id; assert not p.i_id == None
+    #     self.solr_dict[u'bib_ids'] = p.bib_ids
+    #     self.solr_dict[u'bib_ids_filtered'] = p.parseBibIdsFiltered()
+    #     self.solr_dict[u'bib_ids_types'] = p.parseBibIdsTypes()
+    #     self.solr_dict[u'title'] = p.parseTitle()
+    #     self.solr_dict[u'text_genre'] = p.parse_text_genre()
+    #     self.solr_dict[u'object_type'] = p.parse_object_type()
+    #     self.solr_dict[u'bib_titles'] = p.parseBibTitles()
+    #     self.solr_dict[u'bib_titles_all'] = p.bib_titles_all
+    #     self.solr_dict[u'bib_authors'] = p.parseBibAuthors()
+    #     self.solr_dict[u'condition'] = p.parseCondition()
+    #     self.solr_dict[u'decoration'] = p.parseDecoration()
+    #     self.solr_dict[u'fake'] = p.parseFake()
+    #     self.solr_dict[u'graphic_name'] = p.parse_graphic_name()
+    #     self.solr_dict[u'language'] = p.parseLanguage()
+    #     self.solr_dict[u'material'] = p.parseMaterial()
+    #     self.solr_dict[u'msid_region'] = p.parseMsidRegion()
+    #     self.solr_dict[u'msid_settlement'] = p.parseMsidSettlement()
+    #     self.solr_dict[u'msid_institution'] = p.parseMsidInstitution()
+    #     self.solr_dict[u'msid_repository'] = p.parseMsidRepository()
+    #     self.solr_dict[u'msid_idno'] = p.parseMsidIdno()
+    #     self.solr_dict[u'status'] = p.parseStatus()
+    #     self.solr_dict[u'writing'] = p.parseWriting()
+    #     self.log.debug( u'in utils.indexer.Indexer._build_solr_dict(); solr_dict, `%s`' % pprint.pformat(self.solr_dict) )
+    #     return self.solr_dict
+
+    # def _post_solr_update( self ):
+    #     """ Updates existing solr entry.
+    #         Called by update_index_entry(). """
+    #     self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); self.SOLR_URL is: `%s`' % self.SOLR_URL )
+    #     s = solr.Solr( self.SOLR_URL )
+    #     response = s.add( self.solr_dict )
+    #     s.commit()
+    #     s.close()
+    #     self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); post complete; response is: %s' % response )
+    #     # return
+    #     return response
 
     ## remove index entry ##
 
