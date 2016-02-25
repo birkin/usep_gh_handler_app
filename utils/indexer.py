376e6d0664ae251742199acca188aa9fafabb306
birkin <birkin.diana@gmail.com>
2016-02-25 16:06:58 -0500

    moves remove-handling before update-handling to avoid overwrites.

--- utils/indexer.py
+++ utils/indexer.py
@@ -66,58 +66,6 @@ class Indexer( object ):
             raise Exception( unicode(repr(e)) )
         return resp
 
-    # def update_index_entry( self, filename ):
-    #     """ Updates solr index for a new or changed file.
-    #         Called by run_update_index().
-    #         TODO: replace pq extracts with straight lxml or beautifulsoup extracts. """
-    #     self.log.debug( u'in utils.indexer.update_index_entry(); filename, `%s`' % filename )
-    #     full_file_path = u'%s/inscriptions/%s' % ( self.WEBSERVED_DATA_DIR_PATH, filename )
-    #     self._build_solr_dict( full_file_path, self.BIB_XML_PATH )
-    #     self._post_solr_update()
-    #     return
-
-    # def _build_solr_dict( self, inscription_xml_path, bib_xml_path ):
-    #     """ Calls parser to build the solr dict.
-    #         Called by update_index_entry(). """
-    #     p = Parser( inscription_xml_path, bib_xml_path, self.log )
-    #     self.solr_dict[u'id'] = p.i_id; assert not p.i_id == None
-    #     self.solr_dict[u'bib_ids'] = p.bib_ids
-    #     self.solr_dict[u'bib_ids_filtered'] = p.parseBibIdsFiltered()
-    #     self.solr_dict[u'bib_ids_types'] = p.parseBibIdsTypes()
-    #     self.solr_dict[u'title'] = p.parseTitle()
-    #     self.solr_dict[u'text_genre'] = p.parse_text_genre()
-    #     self.solr_dict[u'object_type'] = p.parse_object_type()
-    #     self.solr_dict[u'bib_titles'] = p.parseBibTitles()
-    #     self.solr_dict[u'bib_titles_all'] = p.bib_titles_all
-    #     self.solr_dict[u'bib_authors'] = p.parseBibAuthors()
-    #     self.solr_dict[u'condition'] = p.parseCondition()
-    #     self.solr_dict[u'decoration'] = p.parseDecoration()
-    #     self.solr_dict[u'fake'] = p.parseFake()
-    #     self.solr_dict[u'graphic_name'] = p.parse_graphic_name()
-    #     self.solr_dict[u'language'] = p.parseLanguage()
-    #     self.solr_dict[u'material'] = p.parseMaterial()
-    #     self.solr_dict[u'msid_region'] = p.parseMsidRegion()
-    #     self.solr_dict[u'msid_settlement'] = p.parseMsidSettlement()
-    #     self.solr_dict[u'msid_institution'] = p.parseMsidInstitution()
-    #     self.solr_dict[u'msid_repository'] = p.parseMsidRepository()
-    #     self.solr_dict[u'msid_idno'] = p.parseMsidIdno()
-    #     self.solr_dict[u'status'] = p.parseStatus()
-    #     self.solr_dict[u'writing'] = p.parseWriting()
-    #     self.log.debug( u'in utils.indexer.Indexer._build_solr_dict(); solr_dict, `%s`' % pprint.pformat(self.solr_dict) )
-    #     return self.solr_dict
-
-    # def _post_solr_update( self ):
-    #     """ Updates existing solr entry.
-    #         Called by update_index_entry(). """
-    #     self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); self.SOLR_URL is: `%s`' % self.SOLR_URL )
-    #     s = solr.Solr( self.SOLR_URL )
-    #     response = s.add( self.solr_dict )
-    #     s.commit()
-    #     s.close()
-    #     self.log.debug( u'in utils.indexer.Indexer._post_solr_update(); post complete; response is: %s' % response )
-    #     # return
-    #     return response
-
     ## remove index entry ##
 
     def remove_index_entry( self, filename=None, inscription_id=None ):
@@ -161,19 +109,34 @@ class Indexer( object ):
 
 q = rq.Queue( u'usep', connection=redis.Redis() )
 
+# def run_update_index( files_updated, files_removed ):
+#     """ Creates index jobs (doesn't actually call Indexer() directly.
+#         Triggered by utils.processor.run_xinclude_updater(). """
+#     log = log_helper.setup_logger()
+#     indexer = Indexer( log )
+#     for updated_file_path in files_updated:
+#         if indexer.check_updated_file_path( updated_file_path ):
+#             q.enqueue_call(
+#                 func=u'usep_gh_handler_app.utils.indexer.run_update_entry', kwargs={u'updated_file_path': updated_file_path} )
+#     for removed_file_path in files_removed:
+#         if indexer.check_removed_file_path( removed_file_path ):
+#             q.enqueue_call(
+#                 func=u'usep_gh_handler_app.utils.indexer.run_remove_entry', kwargs={u'removed_file_path': removed_file_path} )
+#     return
+
 def run_update_index( files_updated, files_removed ):
     """ Creates index jobs (doesn't actually call Indexer() directly.
         Triggered by utils.processor.run_xinclude_updater(). """
     log = log_helper.setup_logger()
     indexer = Indexer( log )
-    for updated_file_path in files_updated:
-        if indexer.check_updated_file_path( updated_file_path ):
-            q.enqueue_call(
-                func=u'usep_gh_handler_app.utils.indexer.run_update_entry', kwargs={u'updated_file_path': updated_file_path} )
     for removed_file_path in files_removed:
         if indexer.check_removed_file_path( removed_file_path ):
             q.enqueue_call(
                 func=u'usep_gh_handler_app.utils.indexer.run_remove_entry', kwargs={u'removed_file_path': removed_file_path} )
+    for updated_file_path in files_updated:
+        if indexer.check_updated_file_path( updated_file_path ):
+            q.enqueue_call(
+                func=u'usep_gh_handler_app.utils.indexer.run_update_entry', kwargs={u'updated_file_path': updated_file_path} )
     return
 
 def run_update_entry( updated_file_path ):
