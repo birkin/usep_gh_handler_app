b0c72dbd759e8ec22292d4d19e31b8f6c10751da
birkin <birkin.diana@gmail.com>
2016-03-01 10:29:00 -0500

    makes spaces from tabs.

--- utils/bib_adder.py
+++ utils/bib_adder.py
@@ -8,47 +8,47 @@ from lxml import etree
 
 
 class BibAdder():
-	"""Adds hierarchical bibliography to a given inscription."""
+    """Adds hierarchical bibliography to a given inscription."""
 
-	def __init__(self, solr_url, titles_url, log):
-		self.solr_url = solr_url
-		self.log = log
+    def __init__(self, solr_url, titles_url, log):
+        self.solr_url = solr_url
+        self.log = log
 
-		try:
-			r = requests.get(titles_url)
-			self.titles_xml = etree.fromstring(r.content)
-		except Exception as e:
-			self.log.error( 'Exception in __init__, ```%s```' % unicode(repr(e)) )
-			raise Exception( unicode(repr(e)) )
+        try:
+            r = requests.get(titles_url)
+            self.titles_xml = etree.fromstring(r.content)
+        except Exception as e:
+            self.log.error( 'Exception in __init__, ```%s```' % unicode(repr(e)) )
+            raise Exception( unicode(repr(e)) )
 
-	def addBibl(self, inscription_id):
+    def addBibl(self, inscription_id):
         self.log.debug( 'addBibl inscription_id, `%s`' % inscription_id )
-		try:
-			r = requests.get(self.solr_url + "/select", params={'q':'id:"{}"'.format(inscription_id), 'fl':'bib_ids', 'wt':'json'})
-		except Exception, e:
-			self.log.error( 'Exception on requests select, ```%s```' % unicode(repr(e)) )
-			raise Exception( unicode(repr(e)) )
+        try:
+            r = requests.get(self.solr_url + "/select", params={'q':'id:"{}"'.format(inscription_id), 'fl':'bib_ids', 'wt':'json'})
+        except Exception, e:
+            self.log.error( 'Exception on requests select, ```%s```' % unicode(repr(e)) )
+            raise Exception( unicode(repr(e)) )
 
-		try:
-			doc = r.json()['response']['docs'][0]
-			bib_ids = doc["bib_ids"] if "bib_ids" in doc else []
-		except KeyError:
-			self.log.error("KeyError: Solr returned no results for id: {}".format(inscription_id))
-			return False
+        try:
+            doc = r.json()['response']['docs'][0]
+            bib_ids = doc["bib_ids"] if "bib_ids" in doc else []
+        except KeyError:
+            self.log.error("KeyError: Solr returned no results for id: {}".format(inscription_id))
+            return False
 
-		ids = set()
-		for i in bib_ids:
-			ids.update(etree.XPath("//tei:bibl[@xml:id='"+i+"']/ancestor::tei:bibl/@xml:id", namespaces={"tei":"http://www.tei-c.org/ns/1.0"})(self.titles_xml))
+        ids = set()
+        for i in bib_ids:
+            ids.update(etree.XPath("//tei:bibl[@xml:id='"+i+"']/ancestor::tei:bibl/@xml:id", namespaces={"tei":"http://www.tei-c.org/ns/1.0"})(self.titles_xml))
 
-		update_json = json.dumps([{"id":inscription_id, "bib_ids":{"add":list(ids)}}])
-		self.log.debug("Updating {0} with JSON {1}".format(inscription_id, update_json))
+        update_json = json.dumps([{"id":inscription_id, "bib_ids":{"add":list(ids)}}])
+        self.log.debug("Updating {0} with JSON {1}".format(inscription_id, update_json))
 
-		try:
-			p = requests.post(self.solr_url + "/update", data=update_json, headers={'Content-type':'application/json'})
-			r = requests.get(self.solr_url + "/update?softCommit=true")
-			return True
-		except Exception, e:
-			self.log.error( 'Exception on requests post or followup get, ```%s```' % unicode(repr(e)) )
-			raise Exception( unicode(repr(e)) )
+        try:
+            p = requests.post(self.solr_url + "/update", data=update_json, headers={'Content-type':'application/json'})
+            r = requests.get(self.solr_url + "/update?softCommit=true")
+            return True
+        except Exception, e:
+            self.log.error( 'Exception on requests post or followup get, ```%s```' % unicode(repr(e)) )
+            raise Exception( unicode(repr(e)) )
 
     # end class BibAdder
