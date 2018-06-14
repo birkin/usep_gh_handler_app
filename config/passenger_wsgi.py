5a9dd5c2481ca870cc175a8197d54989d2ba6d0f
birkin <birkin.diana@gmail.com>
2018-06-14 16:24:10 -0400

    handles env-var loading.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -10,7 +10,7 @@
      """
 
 import os, pprint, sys
-
+import shellvars
 
 ## become self-aware, padawan
 current_directory = os.path.dirname( os.path.abspath(__file__) )
@@ -20,6 +20,7 @@ ACTIVATE_FILE = os.path.abspath( u'%s/../../env_usep_gh/bin/activate_this.py' % 
 PROJECT_DIR = os.path.abspath( u'%s/../../usep_gh_handler_app' % current_directory )
 PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
 SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_usep_gh/lib/python2.7/site-packages' % current_directory )
+ENV_SETTINGS_FILE = os.environ['usep_gh__SETTINGS_PATH']  # set in `httpd/passenger.conf`, and `env/bin/activate`
 
 ## virtualenv
 execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )  # file loads environmental variables
@@ -28,5 +29,10 @@ execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )  # file loads environmen
 for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
  if entry not in sys.path:
    sys.path.append( entry )
+
+## load up env vars
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE )
+for ( key, val ) in var_dct.items():
+    os.environ[key.decode('utf-8')] = val.decode('utf-8')
 
 from usep_gh_handler_app.usep_gh_handler import app as application
