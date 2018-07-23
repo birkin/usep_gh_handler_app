c137b9b94434cdd7147f2b0cd61598dabaae51f4
birkin <birkin.diana@gmail.com>
2018-07-23 16:58:43 -0400

    py3-izes script.

--- utils/queue_utils/output_job_queue.py
+++ utils/queue_utils/output_job_queue.py
@@ -4,26 +4,27 @@ import os, pprint
 import redis, rq
 
 
-queue_name = u'usep'
+queue_name = 'usep'
 q = rq.Queue( queue_name, connection=redis.Redis() )
 
-print u'- number of jobs in queue `%s`: %s' % ( queue_name, len(q.jobs) )
+print( '- number of jobs in queue `%s`: %s' % (queue_name, len(q.jobs)) )
 
 for job in q.jobs:
     job_d = {
-        u'_args': job._args,
-        u'_kwargs': job._kwargs,
-        u'_func_name': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info,
-        u'meta': job.meta,
-        u'_result': job._result,
-        u'_status': job._status,
+        '_args': job._args,
+        '_kwargs': job._kwargs,
+        '_func_name': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info,
+        'meta': job.meta,
+        '_result': job._result,
+        '_status': job._status,
         }
-    print u'- job info...'; pprint.pprint( job_d )
-    print u'---'
+    print( '- job info...' )
+    pprint.pprint( job_d )
+    print( '---' )
