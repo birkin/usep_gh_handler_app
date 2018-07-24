c39f23afca85e783d96f85367e3c60e86f551407
birkin <birkin.diana@gmail.com>
2018-07-24 08:17:36 -0400

    py3 updates queue-util.

--- utils/queue_utils/output_failed_queue.py
+++ utils/queue_utils/output_failed_queue.py
@@ -4,31 +4,31 @@ import os, pprint
 import redis, rq
 
 
-TARGET_QUEUE = u'usep'
-queue_name = u'failed'
+TARGET_QUEUE = 'usep'
+queue_name = 'failed'
 q = rq.Queue( queue_name, connection=redis.Redis() )
 
-d = { u'failed_target_count': None, u'jobs': [] }
+d = { 'failed_target_count': None, 'jobs': [] }
 failed_count = 0
 for job in q.jobs:
     if not job.origin == TARGET_QUEUE:
         continue
     failed_count += 1
     job_d = {
-        u'_args': job._args,
-        u'_kwargs': job._kwargs,
-        u'_func_name': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info,
-        u'meta': job.meta,
-        u'_result': job._result,
-        u'_status': job._status,
+        '_args': job._args,
+        '_kwargs': job._kwargs,
+        '_func_name': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info,
+        'meta': job.meta,
+        '_result': job._result,
+        '_status': job._status,
         }
-    d[u'jobs'].append( job_d )
-d[u'failed_target_count'] = failed_count
+    d['jobs'].append( job_d )
+d['failed_target_count'] = failed_count
 pprint.pprint( d )
