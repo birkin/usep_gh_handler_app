1f789aee3a2bafb1243ab81e5aae0ff05bc7cfd0
birkin <birkin.diana@gmail.com>
2018-07-24 10:51:59 -0400

    applies py3 updates.

--- utils/log_helper.py
+++ utils/log_helper.py
@@ -5,56 +5,23 @@
 import datetime, json, logging, os
 
 
-LOG_CONF_JSN = os.environ[u'usep_gh__WRKR_LOG_CONF_JSN']
-
-
+LOG_CONF_JSN = os.environ['usep_gh__WRKR_LOG_CONF_JSN']
 log = logging.getLogger( 'usep_gh_worker_logger' )
 if not logging._handlers:  # true when module accessed by queue-jobs
     logging_config_dct = json.loads( LOG_CONF_JSN )
     logging.config.dictConfig( logging_config_dct )
 
 
-# def setup_logger():
-#     """ Returns a logger to write to a file.
-#         Called by usep_gh_handler.py and processor.py functions. """
-#     LOG_DIR = unicode( os.environ.get(u'usep_gh__LOG_DIR') )
-#     LOG_LEVEL = unicode( os.environ.get(u'usep_gh__LOG_LEVEL') )
-#     filename = u'%s/usep_gh_handler.log' % LOG_DIR
-#     formatter = logging.Formatter( u'[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s' )
-#     logger = logging.getLogger( __name__ )
-#     # logger = logging.getLogger( u'usep_gh_handler' )
-#     level_dict = { u'debug': logging.DEBUG, u'info':logging.INFO }
-#     logger.setLevel( level_dict[LOG_LEVEL] )
-#     file_handler = logging.FileHandler( filename )
-#     file_handler.setFormatter( formatter )
-#     logger.addHandler( file_handler )
-#     logger.debug( u'in utils.log_helper.setup_logger(); log initialized at %s' % unicode(datetime.datetime.now()) )
-#     return logger
-
-
-# def log_envoy_output( envoy_response ):
-#     """ Creates and returns dict of envoy_response attributes.
-#         Called by utils.processor.Puller.call_git_pull(). """
-#     return_dict = {
-#         u'status_code': envoy_response.status_code,  # int
-#         u'std_out': envoy_response.std_out.decode(u'utf-8'),
-#         u'std_err': envoy_response.std_err.decode(u'utf-8'),
-#         u'command': envoy_response.command,  # list
-#         u'history': envoy_response.history  # list
-#         }
-#     log.info( u'in utils.log_helper.log_envoy_output(); envoy_output, `%s`' % return_dict )
-#     return return_dict
-
 def log_envoy_output( envoy_response ):
     """ Creates and returns dict of envoy_response attributes.
         Called by utils.processor.Puller.call_git_pull(). """
     log.debug( 'starting log_envoy_output()' )
     return_dict = {
-        u'status_code': envoy_response.status_code,  # int
-        u'std_out': envoy_response.std_out.decode(u'utf-8'),
-        u'std_err': envoy_response.std_err.decode(u'utf-8'),
-        u'command': envoy_response.command,  # list
-        u'history': envoy_response.history  # list
+        'status_code': envoy_response.status_code,  # int
+        'std_out': envoy_response.std_out,
+        'std_err': envoy_response.std_err,
+        'command': envoy_response.command,  # list
+        'history': envoy_response.history  # list
         }
-    log.info( u'in utils.log_helper.log_envoy_output(); envoy_output, ```%s```' % return_dict )
+    log.info( 'envoy_output, ```%s```' % return_dict )
     return return_dict
